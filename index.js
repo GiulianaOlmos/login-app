7cf749e9621687aaf35c97e778de2f120a0c7733
Giuliana Evangelina Olmos <giulianaolmos@gmail.com>
2020-06-19 16:58:16 -0300

    Test

--- index.js
+++ index.js
@@ -6,28 +6,8 @@ const express = require('express');
 
 const port = process.env.PORT || 8080;
 
-app.set('llave', config.llave);
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
-
-app.post('/autenticar', function(req, res) {
-    if(req.body.usuario === "giuliana" && req.body.contrasena === "estaesamicontrasena"){
-        const payload = {
-            check: true
-        };
-        const token = jwt.sign(payload, app.get('llave'), {
-            expiresIn: 1440
-        });
-        res.json({
-            mensaje: 'Autenticación correcta',
-            token: token
-        })
-    } else {
-        res.json({ mensaje: "Usuario o contraseña incorrectos"})
-    }
-})
 
 app.get('/', function (req, res) {
     res.send('Hola Giuli');
@@ -37,6 +17,13 @@ app.get('/Holis', function (req, res) {
     res.send('Amiga funciona');
 });
 
+app.post('/autenticar', (req, res) => {
+    if(req.body.usuario === "giuliana" && req.body.contrasena === "estaesamicontrasena"){
+        res.send("Vevo te logueaste")
+    } else {
+        res.json({ mensaje: "Usuario o contraseña incorrectos"})
+    }
+})
 
 
 app.listen(port);
