e05475d7a5b36a7a3b3356ffab347cc1118c82fa
Giuliana Evangelina Olmos <giulianaolmos@gmail.com>
2020-06-19 17:32:18 -0300

    Borro codigo

--- index.js
+++ index.js
@@ -39,21 +39,4 @@ app.post('/autenticar', (req, res) => {
     }
 })
 
-app.post('/autenticar', (req, res) => {
-    if(req.body.usuario === "asfo" && req.body.contrasena === "holamundo") {
-        const payload = {
-            check:  true
-        };
-        const token = jwt.sign(payload, app.get('llave'), {
-            expiresIn: 1440
-        });
-        res.json({
-            mensaje: 'Autenticación correcta',
-            token: token
-        });
-    } else {
-        res.json({ mensaje: "Usuario o contraseña incorrectos"})
-    }
-})
-
 app.listen(port);
