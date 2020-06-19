2eb0d5c7cb20b7bac9434cbb313b7b28eda13fc2
Giuliana Evangelina Olmos <giulianaolmos@gmail.com>
2020-06-19 16:24:05 -0300

    Contraseña creada

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+      bodyParser = require('body-parser');
+      jwt = require('jsonwebtoken');
+      config = require('./config/config');
+      app = express();
+
 const port = process.env.PORT || 8080;
+
+app.set('llave', config.llave);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
     res.send('Hola Giuli');
 });
+
 app.get('/Holis', function (req, res) {
     res.send('Amiga funciona');
 });
 
-
+app.post('/autenticar', (req, res) => {
+    if(req.body.usuario === "giuliana" && req.body.contrasena === "estaesamicontrasena"){
+        const payload = {
+            check: true
+        };
+        const token = jwt.sign(payload, app.get('llave'), {
+            expiresIn: 1440
+        });
+        res.json({
+            mensaje: 'Autenticación correcta',
+            token: token
+        })
+    } else {
+        res.json({ mensaje: "Usuario o contraseña incorrectos"})
+    }
+})
 
 app.listen(port);
