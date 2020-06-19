539c735f97332ce1d1ceb86725eabcf7f3b242b7
Giuliana Evangelina Olmos <giulianaolmos@gmail.com>
2020-06-19 17:53:09 -0300

    sin el post

--- index.js
+++ index.js
@@ -22,21 +22,6 @@ app.get('/Holis', function (req, res) {
     res.send('Amiga funciona');
 });
 
-app.post('/autenticar', (req, res) => {
-    if(req.body.usuario === "giuliana" && req.body.contrasena === "estaesamicontrasena"){
-        const payload = {
-            check:  true
-        };
-        const token = jwt.sign(payload, app.get('llave'), {
-            expiresIn: 1440
-        });
-        res.json({
-            mensaje: 'Autenticación correcta',
-            token: token
-        });    
-    } else {
-        res.json({ mensaje: "Usuario o contraseña incorrectos"})
-    }
-})
+
 
 app.listen(port);
